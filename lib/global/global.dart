949bfca3d61dd85b9d804db9590e7428d6320fc9
Namdn <namdn@ldcc.vn>
2024-06-13 21:54:07 +0700

    init firebase

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
+import 'package:translate_app/firebase_options.dart';
 
 class Global {
   static Future<void> initialApp() async {
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarBrightness: Brightness.dark,
     ));
+
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:translate_app/config.dart';
+import 'package:translate_app/global/global.dart';
 
 import 'app/app.dart';
 
 void main() async {
   setAppDevelopment();
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  await Global.initialApp();
+
   runApp(const App());
 }
